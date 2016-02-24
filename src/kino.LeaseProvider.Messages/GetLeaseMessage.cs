06d082a44ca5799d3cfd503e31e698f24b53773e
iiwaasnet <givasena@gmail.com>
2016-02-24 21:27:34 +0100

    Added Messages library

---
+++ src/kino.LeaseProvider.Messages/GetLeaseMessage.cs
@@ -0,0 +1,15 @@
+﻿using kino.Core.Framework;
+using kino.Core.Messaging;
+
+namespace kino.LeaseProvider.Messages
+{
+    public class GetLeaseMessage : Payload
+    {
+        private static readonly byte[] MessageVersion = "1.0".GetBytes();
+        private static readonly byte[] MessageIdentity = "LP.GETLEASE".GetBytes();
+
+        public override byte[] Version => MessageVersion;
+
+        public override byte[] Identity => MessageIdentity;
+    }
+}
---
+++ src/kino.LeaseProvider/LeaseProviderActor.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using kino.Actors;
+using kino.Consensus;
+using kino.Consensus.Configuration;
+using kino.Core.Connectivity;
+using kino.Core.Diagnostics;
+using kino.Core.Messaging;
+using kino.LeaseProvider.Messages;
+
+namespace kino.LeaseProvider
+{
+    public class LeaseProviderActor : Actor
+    {
+        private readonly IRoundBasedRegister register;
+        private readonly IBallotGenerator ballotGenerator;
+        private readonly LeaseConfiguration config;
+        private readonly ISynodConfiguration synodConfig;
+        private readonly ILogger logger;
+
+        public LeaseProviderActor(IRoundBasedRegister register,
+                                  IBallotGenerator ballotGenerator,
+                                  LeaseConfiguration config,
+                                  ISynodConfiguration synodConfig,
+                                  ILogger logger)
+        {
+            this.register = register;
+            this.ballotGenerator = ballotGenerator;
+            this.config = config;
+            this.synodConfig = synodConfig;
+            this.logger = logger;
+        }
+
+        [MessageHandlerDefinition(typeof(GetLeaseMessage))]
+        public Task<ActorResult> GetLease(IMessage message)
+        {
+        }
+    }
+}
