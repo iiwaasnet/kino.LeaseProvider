b2b9b2ea2f63f72837146abd7002e425aba57a09
iiwaasnet <givasena@gmail.com>
2017-05-12 20:59:50 +0200

    Added Autofac integration package

---
+++ src/Integration/Autofac.kino.LeaseProvider/LeaseProviderModule.cs
@@ -0,0 +1,70 @@
+﻿using kino.Actors;
+using kino.Actors.Diagnostics;
+using kino.Consensus;
+using kino.Consensus.Configuration;
+using kino.LeaseProvider.Actors;
+using kino.LeaseProvider.Configuration;
+using kino.Messaging;
+using SynodConfiguration = kino.Consensus.Configuration.SynodConfiguration;
+
+namespace Autofac.kino.LeaseProvider
+{
+    public class LeaseProviderModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule<KinoModule>();
+
+            builder.Register(c => c.Resolve<LeaseProviderServiceConfiguration>().LeaseProvider)
+                   .AsSelf()
+                   .SingleInstance();
+
+            builder.Register(c => c.Resolve<LeaseProviderServiceConfiguration>().LeaseProvider.Synod)
+                   .AsSelf()
+                   .SingleInstance();
+
+            builder.Register(c => c.Resolve<LeaseProviderServiceConfiguration>().LeaseProvider.Lease)
+                   .AsSelf()
+                   .SingleInstance();
+
+            builder.RegisterType<SynodConfigurationProvider>()
+                   .As<ISynodConfigurationProvider>()
+                   .SingleInstance();
+
+            builder.RegisterType<SynodConfiguration>()
+                   .As<ISynodConfiguration>()
+                   .SingleInstance();
+
+            builder.RegisterType<IntercomMessageHub>()
+                   .As<IIntercomMessageHub>()
+                   .SingleInstance();
+
+            builder.RegisterType<BallotGenerator>()
+                   .As<IBallotGenerator>()
+                   .SingleInstance();
+
+            builder.RegisterType<global::kino.LeaseProvider.LeaseProvider>()
+                   .As<ILeaseProvider>()
+                   .SingleInstance();
+
+            builder.RegisterType<ProtobufMessageSerializer>()
+                   .As<IMessageSerializer>()
+                   .SingleInstance();
+
+            builder.RegisterType<LeaseProviderActor>()
+                   .As<IActor>()
+                   .SingleInstance();
+
+            builder.RegisterType<InstanceDiscoveryActor>()
+                   .As<IActor>()
+                   .SingleInstance();
+
+            builder.RegisterType<InstanceBuilderActor>()
+                   .As<IActor>()
+                   .SingleInstance();
+            builder.RegisterType<ExceptionHandlerActor>()
+                   .As<IActor>()
+                   .SingleInstance();
+        }
+    }
+}
